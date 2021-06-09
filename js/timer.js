a8990b6bc51ef1e1a3448c9643d740d9133332d7
Oleksii Nesterenko <71211789+Nashweel@users.noreply.github.com>
2021-06-09 17:59:34 +0300

    Change js file

--- js/timer.js
+++ js/timer.js
@@ -3,28 +3,14 @@ new CountdownTimer({
     targetDate: new Date('Jul 17, 2019'),
   });
 
-  
-  /*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const refs = {
+  Days: document.querySelector('.value[data-value="days"]'),
+  Hours: document.querySelector('.value[data-value="hours"]'),
+  Mins: document.querySelector('.value[data-value="mins"]'),
+  Secs: document.querySelector('.value[data-value="secs"]')
+  };
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((time % (1000 * 60)) / 1000);
